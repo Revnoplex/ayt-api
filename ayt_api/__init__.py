9b6d1674af9f316b275533abc7584088d6852f9c
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2023-01-02 21:03:18 +1030

    update copyright year

--- ayt_api/__init__.py
+++ ayt_api/__init__.py
@@ -4,5 +4,5 @@ from .exceptions import *
 __title__ = "ayt-api"
 __author__ = "Revnoplex"
 __license__ = "MIT"
-__copyright__ = "Copyright (c) 2022 Revnoplex"
+__copyright__ = "Copyright (c) 2022-2023 Revnoplex"
 __version__ = "0.1.1"
