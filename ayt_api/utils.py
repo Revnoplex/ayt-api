e5965da29c4d0bc0178220f24a98da9f7b57d61f
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2023-07-17 21:48:20 +1000

    Reworked video id parsing with urllib.parse library and added playlist id parsing

--- ayt_api/utils.py
+++ ayt_api/utils.py
@@ -1,17 +1,46 @@
-def strip_video_id(url: str) -> str:
-    """supported urls:
-    https://www.youtube.com/watch?v=ID
+import pathlib
+from urllib import parse
 
-    https://www.youtube.com/v/ID
 
-    https://youtu.be/ID"
+def extract_video_id(url: str) -> str:
+    """
+    This should work for every url listed here:
+    https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486#file-activeyoutubeurlformats-txt
+    and more.
     Args:
         url (str): The url to strip the id from
     Returns:
         str: The video id with the rest of the url removed
     """
-    if url.split('/')[2] == 'youtu.be':
-        return url.split('/')[3].split("?")[0].split('&')[0]
+    components = parse.urlparse(url.replace("&", "?", 1) if "?" not in url else url)
+    queries = parse.parse_qs(components.query)
+    encoded_query_matches = {'u', 'url'}.intersection(set(queries.keys()))
+    if 'v' in queries:
+        return queries["v"][0]
+    elif encoded_query_matches:
+        return extract_video_id(parse.unquote(queries[encoded_query_matches.pop()][0]))
+    elif components.netloc == "i.ytimg.com":
+        return pathlib.Path(components.path).parts[2]
     else:
-        slash_index = 4 if url.split("/")[3] == "v" or url.split("/")[3] == "embed" else 3
-        return url.split("/")[slash_index].replace("watch?v=", "").split("&")[0].split("?")[0]
+        return pathlib.Path(components.path).name
+
+
+def extract_playlist_id(url: str) -> str:
+    """
+    This should work with the following urls
+
+    Don't expect this to work on YouTube mixes
+    Args:
+        url (str): The url to strip the id from
+    Returns:
+        str: The playlist id with the rest of the url removed
+    """
+    components = parse.urlparse(url.replace("&", "?", 1) if "?" not in url else url)
+    queries = parse.parse_qs(components.query)
+    encoded_query_matches = {'u', 'url'}.intersection(set(queries.keys()))
+    if 'list' in queries:
+        return queries["list"][0]
+    elif encoded_query_matches:
+        return extract_playlist_id(parse.unquote(queries[encoded_query_matches.pop()][0]))
+    else:
+        return pathlib.Path(components.path).name
