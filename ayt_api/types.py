875c82dbfcbf37ed33aa2bdea8fbf8d79151fe0d
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2023-07-17 21:44:07 +1000

    List of changes to types.py: - Added chapter parsing with new VideoChapter class and other related helper functions - Converted Some classes to dataclasses - updated internal documentation

--- ayt_api/types.py
+++ ayt_api/types.py
@@ -1,35 +1,29 @@
 import datetime
+import re
+from dataclasses import dataclass
 from typing import Union, Optional
 import isodate
 from .exceptions import MissingDataFromMetadata
 
 
+@dataclass
 class YoutubeThumbnail:
     """Data for an individual YouTube thumbnail.
     Attributes:
-        data (Optional[dict]):
-            The raw thumbnail data
         url (Optional[str]): The file url for the thumbnail
         width (Optional[int]): The amount of horizontal pixels in the thumbnail
         height (Optional[int]): The amount of vertical pixels in the thumbnail
         resolution (str): The Width x Height of the thumbnail
     """
-    def __init__(self, data: dict):
-        """
-        Args:
-            data (dict): The raw thumbnail data
-        """
-        self.data = data
-        self.url: Optional[str] = data.get("url")
-        self.width: Optional[int] = data.get("width")
-        self.height: Optional[int] = data.get("height")
+    url: Optional[str]
+    width: Optional[int]
+    height: Optional[int]
+
+    def __post_init__(self):
         self.resolution = "{}x{}".format(self.width, self.height)
 
     def __str__(self):
         return self.url
-
-    def __repr__(self):
-        return f"YoutubeThumbnail({self.url}, {self.width},{self.height})"
 
 
 class YoutubeThumbnailMetadata:
@@ -50,92 +44,75 @@ class YoutubeThumbnailMetadata:
 
     @property
     def default(self) -> Optional[YoutubeThumbnail]:
-        """The default video thumbnail. Could be None.
+        """The default video thumbnail. The value is not guaranteed and could be `None`.
+        This is the most guaranteed to actually return a thumbnail
         Returns:
-            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be None"""
+            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be `None`"""
         if self.metadata.get("default") is not None:
-            return YoutubeThumbnail(self.metadata["default"])
+            return YoutubeThumbnail(**self.metadata["default"])
 
     @property
     def medium(self) -> Optional[YoutubeThumbnail]:
-        """The medium video thumbnail. Could be None
+        """The medium video thumbnail. The value is not guaranteed and could be `None`.
         Returns:
-            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be None"""
+            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be `None`"""
         if self.metadata.get("medium") is not None:
-            return YoutubeThumbnail(self.metadata["medium"])
+            return YoutubeThumbnail(**self.metadata["medium"])
 
     @property
     def high(self) -> Optional[YoutubeThumbnail]:
-        """The high video thumbnail. Could be None
+        """The high video thumbnail. The value is not guaranteed and could be `None`.
         Returns:
-            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be None"""
+            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be `None`"""
         if self.metadata.get("high") is not None:
-            return YoutubeThumbnail(self.metadata["high"])
+            return YoutubeThumbnail(**self.metadata["high"])
 
     @property
     def standard(self) -> Optional[YoutubeThumbnail]:
-        """The standard video thumbnail. Could be None
+        """The standard video thumbnail. The value is not guaranteed and could be `None`.
         Returns:
-            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be None"""
+            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be `None`"""
         if self.metadata.get("standard") is not None:
-            return YoutubeThumbnail(self.metadata["standard"])
+            return YoutubeThumbnail(**self.metadata["standard"])
 
     @property
     def maxres(self) -> Optional[YoutubeThumbnail]:
-        """The maximum resolution video thumbnail. Could be None
+        """The maximum resolution video thumbnail. The value is not guaranteed and could be `None`.
         Returns:
-            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be None"""
+            Optional[YoutubeThumbnail]: A YouTube thumbnail object. Could be `None`"""
         if self.metadata.get("maxres") is not None:
-            return YoutubeThumbnail(self.metadata["maxres"])
+            return YoutubeThumbnail(**self.metadata["maxres"])
 
 
-
+@dataclass
 class LocalName:
     """Represents the video title and description in a local language if available
-    Attributes:
-        data (dict): The raw data associated with the local text
-        language (Optional[str]): The language code
-        title (str): The title in a local language
-        description (str): The description in a local language
+        Attributes:
+            language (Optional[str]): The language code
+            title (str): The title in a local language
+            description (str): The description in a local language
     """
-    def __init__(self, data: dict, lang_key: str = None):
-        """
-        Args:
-            data (dict): The raw local text data
-            lang_key (str): The language code
-        """
-        try:
-            self.data = data
-            self.language = lang_key
-            self.title: str = data["title"]
-            self.description: str = data["description"]
-        except KeyError as missing_snippet_data:
-            raise MissingDataFromMetadata(str(missing_snippet_data), data, missing_snippet_data)
+    title: str
+    description: str
+    language: str = None
 
 
+@dataclass
 class RegionRestrictions:
     """Represents information about the countries where a video is (or is not) viewable.
     Attributes:
-        data (dict): The raw data about what regions are blocked or allowed
-
-        allowed (Optional[list[str]]): The countries that are allowed to view the video. Could be None
-
-        blocked (Optional[list[str]]): The countries that are blocked from viewing the video. Could be None"""
-    def __init__(self, data: dict):
-        """
-        Args:
-            data (dict): The raw regionRestriction data
-        """
-        self.data = data
-        self.allowed: Optional[list[str]] = self.data.get("allowed")
-        self.blocked: Optional[list[str]] = self.data.get("blocked")
+        allowed (Optional[list[str]]): The countries that are allowed to view the video. Could be `None`
+        blocked (Optional[list[str]]): The countries that are blocked from viewing the video. Could be `None`
+    """
+    allowed: list[str] = None
+    blocked: list[str] = None
 
 
 class ContentRating:
     """Specifies the ratings that the video received under various rating schemes.
 
         There are many attributes for each rating system, only 1 or 2 (if there is a reason) will be available,
-        the rest will be None or all if there is no restrictions set.
+        the rest will be `None` or all if there is no restrictions set.
     Attributes:
         acb (Optional[str]): The video's Australian Classification Board (ACB) or Australian Communications and Media
             Authority (ACMA) rating. ACMA ratings are used to classify children's television programming.
@@ -214,26 +191,17 @@ class ContentRating:
         self.youtube: Optional[str] = data.get("ytRating")
 
 
+@dataclass
 class RecordingLocation:
     """The geolocation information associated with the video. if specified by the video uploader
     Attributes:
-        data (dict): The raw geolocation data used to construct this class
         latitude (float): Latitude in degrees.
         longitude (float): Longitude in degrees.
         altitude (float): Altitude above the reference ellipsoid, in meters.
     """
-    def __init__(self, data: dict):
-        """
-        Args:
-            data (dict): The raw geolocation data
-        """
-        try:
-            self.data = data
-            self.latitude: float = self.data["latitude"]
-            self.longitude: float = self.data["longitude"]
-            self.altitude: float = self.data["altitude"]
-        except KeyError as missing_snippet_data:
-            raise MissingDataFromMetadata(str(missing_snippet_data), data, missing_snippet_data)
+    latitude: float
+    longitude: float
+    altitude: float
 
 
 class RecordingDetails:
@@ -254,7 +222,7 @@ class RecordingDetails:
         if data.get("location") is None:
             self.location: Optional[RecordingLocation] = None
         else:
-            self.location: Optional[RecordingLocation] = RecordingLocation(data["location"])
+            self.location: Optional[RecordingLocation] = RecordingLocation(**data["location"])
         if data.get("recordingDate") is None:
             self.date: Optional[datetime.datetime] = None
         else:
@@ -384,11 +352,36 @@ class TagSuggestion:
             return f'Tag Suggestion: "{self.tag}"'
 
 
-class ABCVideoMetadata:
-    pass
+@dataclass
+class VideoChapter:
+    """
+    The start time, duration and name of a YouTube video chapter
+    Attributes:
+        start (int): the start time of the chapter in seconds
+        duration: (int): the length of the chapter in seconds
+        name (str): The name of the chapter
+    """
+    start: datetime.timedelta
+    duration: datetime.timedelta
+    name: str
+
+    def __str__(self):
+        return self.name
 
 
-class YoutubeVideoMetadata(ABCVideoMetadata):
+@dataclass
+class BaseVideo:
+    metadata: dict
+    call_url: str
+    id: str
+    url: str
+    title: str
+    description: str
+    thumbnails = YoutubeThumbnailMetadata
+    visibility: Optional[str]
+
+
+class YoutubeVideoMetadata(BaseVideo):
     """A data class containing video data such as the title, id, description, channel, etc.
         Attributes:
             metadata (dict): The raw API response used to construct this class
@@ -427,7 +420,7 @@ class YoutubeVideoMetadata(ABCVideoMetadata):
             licensed_content (bool): Indicates whether the video represents licensed content, which means that the
                 content was uploaded to a channel linked to a YouTube content partner and then claimed by that partner.
             region_restrictions (Optional[RegionRestrictions]): contains information about the countries where a video
-                is (or is not) viewable. Can be None
+                is (or is not) viewable. Can be `None`
             content_rating (ContentRating): Specifies the ratings that the video received under various rating schemes.
             age_restricted (bool): Whether the video is age restricted or not.
             projection (str): Specifies the projection format of the video. (example: 360 or rectangular)
@@ -447,12 +440,12 @@ class YoutubeVideoMetadata(ABCVideoMetadata):
                 current "made for kids" status of the video.
             view_count (int): The number of times the video has been viewed.
             like_count (Optional[int]): The number of users who have indicated that they liked the video.
-            comment_count (Optional[int]): The number of comments on the video. This attribute is None if disabled
+            comment_count (Optional[int]): The number of comments on the video. This attribute is `None` if disabled
             embed_html (Optional[str]): An <iframe> tag that embeds a player that plays the video.
             embed_height (Optional[str]): The height of the embedded player returned to the embed_html attribute.
-                Is None unless the height and aspect ratio is known
+                Is `None` unless the height and aspect ratio is known
             embed_width (Optional[str]): The width of the embedded player returned to the embed_html attribute.
-                Is None unless the width and aspect ratio is known
+                Is `None` unless the width and aspect ratio is known
             topic_categories (Optional[list[str]]): A list of Wikipedia URLs that provide a high-level description of
                 the video's content.
             recording_details (RecordingDetails): Encapsulates information about the location, date and address where
@@ -506,7 +499,7 @@ class YoutubeVideoMetadata(ABCVideoMetadata):
             if self.snippet.get("localized") is None:
                 self.localized: Optional[LocalName] = None
             else:
-                self.localised: Optional[LocalName] = LocalName(self.snippet["localized"])
+                self.localised: Optional[LocalName] = LocalName(**self.snippet["localized"])
             self.localized = self.localised
             self.default_audio_language: Optional[str] = self.snippet.get("defaultAudioLanguage")
             self.duration = isodate.parse_duration(self.content_details["duration"])
@@ -523,7 +516,7 @@ class YoutubeVideoMetadata(ABCVideoMetadata):
                 self.region_restrictions: Optional[RegionRestrictions] = None
             else:
                 self.region_restrictions: Optional[RegionRestrictions] = \
-                    RegionRestrictions(self.content_details["regionRestriction"])
+                    RegionRestrictions(**self.content_details["regionRestriction"])
             self.content_rating = ContentRating(self.content_details["contentRating"])
             self.age_restricted = bool(self.content_rating.youtube)
             self.projection: Optional[str] = self.content_details.get("projection")
@@ -590,14 +583,56 @@ class YoutubeVideoMetadata(ABCVideoMetadata):
             else:
                 self.localisations: Optional[list[LocalName]] = []
                 for localisation in self.raw_localisations.items():
-                    self.localisations.append(LocalName(localisation[1], localisation[0]))
+                    self.localisations.append(LocalName(**localisation[1], language=localisation[0]))
             self.localizations = self.localisations
 
         except KeyError as missing_snippet_data:
             raise MissingDataFromMetadata(str(missing_snippet_data), metadata, missing_snippet_data)
+        except TypeError as missing_snippet_data:
+            missing_str = f"{str(missing_snippet_data).split('arguments: ')[-1]} in " \
+                          f"{str(missing_snippet_data).split('.')[0]}"
+            raise MissingDataFromMetadata(missing_str, metadata, missing_snippet_data)
+
+    @property
+    def chapters(self) -> Optional[list[VideoChapter]]:
+        """A list of chapters the video has if any otherwise just an empty list
+        Returns:
+            chapters (list[VideoChapter]): A list of chapters the video has if any otherwise `None`
+        """
+        found_chapters = []
+        for line in reversed(self.description.splitlines()):
+            # regex is from https://stackoverflow.com/a/11067610
+            parsed = re.search(r'(?:([0-5]?[0-9]):)?([0-5]?[0-9]):([0-5][0-9])', line)
+            if parsed is not None:
+                raw_stamp = parsed.group()
+                split_timestamp = raw_stamp.split(":")
+                seconds = 0
+                for index, part in enumerate(reversed(split_timestamp)):
+                    seconds += int(part) * 60 ** index
+                start = datetime.timedelta(seconds=seconds)
+                end = found_chapters[-1][0] if len(found_chapters) > 0 else self.duration
+                duration = end - start
+                found_chapters.append((start, duration, line.replace(raw_stamp, "", 1).strip()))
+        return [VideoChapter(*chapter_data) for chapter_data in reversed(found_chapters)] if found_chapters else None
+
+    def current_chapter(self, position: datetime.timedelta) -> Optional[VideoChapter]:
+        """
+        Gets the current chapter based on the position provided
+        Args:
+            position (datetime.timedelta): The position of the video to get the current chapter from
+        Returns:
+            Optional[VideoChapter]: The current video chapter at that position of the video. Returns `None` if either
+                                    the video doesn't have any chapters or the position is greater than the duration of
+                                    the video or is negative
+        """
+        if self.chapters:
+            for idx, chapter in enumerate(self.chapters):
+                if chapter.start <= position < chapter.start + chapter.duration or \
+                        (idx+1 == len(self.chapters) and position == self.duration):
+                    return chapter
 
 
-class PlaylistVideoMetadata(ABCVideoMetadata):
+class PlaylistVideoMetadata(BaseVideo):
     """A data class for videos in a playlist
     Attributes:
         metadata (dict): The raw metadata from the API call used to construct this class
@@ -698,7 +733,7 @@ class YoutubePlaylistMetadata:
         description (str): The description of the playlist
         thumbnails (YoutubeThumbnailMetadata): The available thumbnails the playlist has
         channel_title: (Optional[str]) The name of the channel that created the playlist
-        default_language (Optional[str]): The default language the video is set in. Can be None
+        default_language (Optional[str]): The default language the video is set in. Can be `None`
         localised (Optional[LocalName]): The localised language of the title and description of the video
         localized (Optional[LocalName]): an alias of localised
         visibility (Optional[str]): The video's privacy status. Can be private, public or unlisted
@@ -740,7 +775,7 @@ class YoutubePlaylistMetadata:
             if self.snippet.get("localized") is None:
                 self.localised: Optional[LocalName] = None
             else:
-                self.localised: Optional[LocalName] = LocalName(self.snippet["localized"])
+                self.localised: Optional[LocalName] = LocalName(**self.snippet["localized"])
                 self.localized = self.localised
             self.visibility: Optional[str] = self.status.get("privacyStatus")
             self.item_count: Optional[int] = self.content_details.get("itemCount")
@@ -750,7 +785,7 @@ class YoutubePlaylistMetadata:
             else:
                 self.localisations: Optional[list[LocalName]] = []
                 for localisation_name, localisation_value in self.raw_localisations.items():
-                    self.localisations.append(LocalName(localisation_value, localisation_name))
+                    self.localisations.append(LocalName(**localisation_value, language=localisation_name))
             self.localizations = self.localisations
         except KeyError as missing_snippet_data:
             raise MissingDataFromMetadata(str(missing_snippet_data), metadata, missing_snippet_data)
