34be1755b1d559b38b46d610ad8744b58b5c6144
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2024-10-17 21:06:28 +1100

    test if files outside docs/ is accessable

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,21 @@
 # Configuration file for the Sphinx documentation builder.
+import codecs
+import os
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 # -- Project information
 
@@ -6,8 +23,8 @@ project = 'AYT API'
 copyright = '2024 Revnoplex'
 author = 'Revnoplex'
 
-release = '0.3'
-version = '0.3.0'
+version = get_version('../../ayt_api/__init__.py')
+release = ".".join(version.split(".")[:-1])
 
 # -- General configuration
 
