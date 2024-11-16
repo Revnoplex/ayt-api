2cc3445f713d9f918f7ad7928cfe03f13eddf6d3
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2024-11-16 12:47:44 +1100

    bump version in advance

--- ayt_api/__init__.py
+++ ayt_api/__init__.py
@@ -10,7 +10,7 @@ __title__ = "ayt-api"
 __author__ = "Revnoplex"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2022-2023 Revnoplex"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 
 class VersionInfo(_NamedTuple):
